37fc3692e8be4c8997c7df113198b822d48d9900
yohannesluminari <y.luminari@gmail.com>
2024-01-30 16:39:08 +0100

    L2

--- m1/s2/g2/L2/scripts.js
+++ m1/s2/g2/L2/scripts.js
@@ -115,8 +115,9 @@ console.log("il tipo di dato è un " + typeof number);
 /* ESERCIZIO 9
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
-let pari = 6;
-if ( a % 2 == 0){
+
+let pari = 4;
+if ( pari % 2 == 0){
   console.log(pari+ ": è un numero pari " );
 }else{
   console.log(pari+ ": è un numero dispari " );
@@ -124,17 +125,17 @@ if ( a % 2 == 0){
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-/* ESERCIZIO 10
-  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
-  let val = 7
-  if (val < 10) {
+// ESERCIZIO 10
+// Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
+  let val = 10;
+  if (val < 10 && val >= 10) {
       console.log("Meno di 10");
-    } else if (val < 5) {
+    } else if (val < 5 ) {
       console.log("Meno di 5");
     } else {
       console.log("Uguale a 10 o maggiore");
     }
-*/
+
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
