a276491120a5c3a14a980828bf26959f0299200b
yohannesluminari <y.luminari@gmail.com>
2024-01-30 09:12:44 +0100

    script

--- m1/s2/g1/js/script.js
+++ m1/s2/g1/js/script.js
@@ -14,3 +14,8 @@ if(anni >= 18 && anni <= 120){
 console.log(!falase) // ! -  contrario di un output - true
 console.log(!true) // false
 console.log(!(anni >= 18) );
+console.log( 30 == '30') // TRUE basta che sia uguale il valore
+console.log( 30 === '30') // FALSE devono essere uguali sia il valore che il tipo di dato
+
+console.log( 30 != '30') // false i tipi di dato sono uguali - Number - Number (solo 1 =)
+console.log( 30 !== '30') // TRUE i tipi di dato sono diversi - Number - String (2 =)
