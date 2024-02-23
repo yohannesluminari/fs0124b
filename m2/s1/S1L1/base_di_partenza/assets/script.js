9309999eaba4e472af18cb8c09dad5496301a67c
yohannesluminari <y.luminari@gmail.com>
2024-02-23 16:34:11 +0100

    M2S1L1

--- m2/s1/S1L1/base_di_partenza/assets/script.js
+++ m2/s1/S1L1/base_di_partenza/assets/script.js
@@ -3,11 +3,11 @@ window.addEventListener('scroll', function() {
     let startButton = document.getElementById('start');
     let scrollPosition = window.scrollY;
 
-    if (scrollPosition > 100) { // Modifica 100 a seconda di quando vuoi che accada il cambio
+    if (scrollPosition > 500) { 
         header.style.backgroundColor = 'white';
         startButton.style.backgroundColor = 'green';
     } else {
         header.style.backgroundColor = 'transparent';
-        startButton.style.backgroundColor = 'transparent';
+        startButton.style.backgroundColor = '#191919';
     }
 });
