6529bcf2316b1b57a80e86d60131f6591afd0e7a
yohannesluminari <y.luminari@gmail.com>
2024-03-01 16:11:07 +0100

    home

---
+++ m2/s2/M2S2L5/script/script.js
@@ -0,0 +1,16 @@
+const filmContainers = [...document.querySelectorAll('.film-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+filmContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
