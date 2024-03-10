70c7343dbe212d8a05c50348f645115d2d8afe04
yohannesluminari <y.luminari@gmail.com>
2024-03-10 12:19:02 +0100

    a

--- m2/s3/L5/js/delete-card.js
+++ m2/s3/L5/js/delete-card.js
@@ -36,3 +36,40 @@ function deleteCardOnClick() {
 }
 
 deleteCardOnClick();
+
+
+
+
+// const deleteButtons = document.querySelectorAll('.delete-button');
+// deleteButtons.forEach(button => {
+//     button.addEventListener('click', async function() {
+
+//         const card = this.closest('.col');
+        
+//         const phoneId = card.dataset.phoneId;
+
+//         if (phoneId === 1) {
+//             try {
+//                 const response = await fetch(`https://striveschool-api.herokuapp.com/api/product/${phoneId}`, {
+//                     method: 'DELETE',
+//                     headers: {
+//                         'Content-Type': 'application/json',
+//                         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhYzk0YzJkN2IxMTAwMTkwZTZkYjgiLCJpYXQiOjE3MDk5MDQ3MTgsImV4cCI6MTcxMTExNDMxOH0.XkaYR5emkds_eFxe7-sV7h2pAdxheVALxtpOGk6yeFg"
+//                     }
+//                 });
+
+//                 if (response.ok) {
+                    
+//                     card.remove();
+//                     console.log('Card telefono con id = 1 eliminata con successo.');
+//                 } else {
+//                     console.error('Si è verificato un errore durante l\'eliminazione della card telefono con id = 1.');
+//                 }
+//             } catch (error) {
+//                 console.error('Si è verificato un errore durante l\'invio della richiesta DELETE:', error);
+//             }
+//         } else {
+//             console.log('La card telefono con id diverso da 1 non può essere eliminata.');
+//         }
+//     });
+// });
