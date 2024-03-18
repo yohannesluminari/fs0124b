23269077c5d93c18d64aa9bbb558f98822702eff
yohannesluminari <y.luminari@gmail.com>
2024-03-18 09:09:30 +0100

    s9l1 typescript

--- m3/s3/TypeScript_Introdzuobe/struttureDati/index.js
+++ m3/s3/TypeScript_Introdzuobe/struttureDati/index.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //LEZIONE 2
 var Days;
 (function (Days) {
@@ -15,13 +30,50 @@ var Studente = /** @class */ (function () {
     function Studente(_nome, _cognome) {
         this.nome = _nome;
         this.cognome = _cognome;
-        getStudente();
-        string;
-        {
-            return "".concat(this.nome, "  ").concat(this.cognome);
-        }
     }
+    Studente.prototype.getStudente = function () {
+        return "".concat(this.nome, "  ").concat(this.cognome);
+    };
+    Studente.prototype.numeroRandom = function () {
+        return Math.floor(Math.random() * 11);
+    };
+    Studente.prototype.somma = function (num1) {
+        return num1 + this.numeroRandom();
+    };
     return Studente;
 }());
-var nuovoStudnete = new Studente('Mario', 'Rossi');
-console.log(nuovoStudnete.getStudente());
+var nuovoStudente = new Studente('Mario', 'Rossi');
+console.log(nuovoStudente.getStudente());
+console.log(nuovoStudente.somma(15));
+//SOTTOCLASSE
+var Utente = /** @class */ (function (_super) {
+    __extends(Utente, _super);
+    function Utente(_nome, _cognome, _eta) {
+        var _this = _super.call(this, _nome, _cognome) || this;
+        _this.eta = _eta;
+        return _this;
+    }
+    Utente.prototype.getStudente = function () {
+        return "".concat(this.nome, " ").concat(this.cognome, " di anni ").concat(this.eta);
+    };
+    return Utente;
+}(Studente));
+var nuovoUtente = new Utente('Luca', 'Rossi', 45);
+console.log(nuovoUtente.getStudente());
+//RELAZIONI 
+var Phone = /** @class */ (function () {
+    function Phone(_numero) {
+        this.numero = _numero;
+    }
+    return Phone;
+}());
+var UtentePhone = /** @class */ (function () {
+    function UtentePhone(_nome, _cognome, _numeri) {
+        this.nome = _nome;
+        this.cognome = _cognome;
+        this.numeri = _numeri;
+    }
+    return UtentePhone;
+}());
+var nuovoUtentePhone = new UtentePhone('Mario', 'Rossi', [29449292, 674727272, 9834394389]);
+console.log(nuovoUtentePhone);
