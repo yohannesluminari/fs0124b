372da4974f86b14153ec31d711d6a2252bd8f931
yohannesluminari <y.luminari@gmail.com>
2024-03-19 15:47:11 +0100

    g2l2

--- m3/s1/g2/s2l2/src/app/app.module.ts
+++ m3/s1/g2/s2l2/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Component1Component } from './components/component1/component1.component';
 import { Component3Component } from './component3/component3.component';
+import { Component2Component } from './component2/component2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Component1Component,
-    Component3Component
+    Component3Component,
+    Component2Component
   ],
   imports: [
     BrowserModule,
