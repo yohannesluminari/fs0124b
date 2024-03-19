ff34fbdc00388da1807e56085b3d9dd204f01b53
yohannesluminari <y.luminari@gmail.com>
2024-03-19 15:24:44 +0100

    g2l2

---
+++ m3/s1/g2/s2l2/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Component1Component } from './components/component1/component1.component';
+import { Component3Component } from './component3/component3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Component1Component,
+    Component3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
