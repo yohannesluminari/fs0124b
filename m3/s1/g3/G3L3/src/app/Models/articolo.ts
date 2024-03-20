4a8e3755c71a5f792988cb50bfeeec66e1a07e7f
yohannesluminari <y.luminari@gmail.com>
2024-03-20 15:50:52 +0100

    G3L3 NON MI FUZNIONA LA FETC

---
+++ m3/s1/g3/G3L3/src/app/Models/articolo.ts
@@ -0,0 +1,8 @@
+export interface iArticolo {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+  tags: string[];
+  active: boolean;
+}
---
+++ m3/s1/g3/G3L3/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { iArticolo } from './Models/articolo';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+
+  postArr:iArticolo[] = [];
+
+  ngOnInit(){
+
+    fetch('../assets/pizze.json')
+    .then(articoli => articoli.json())
+    .then((articolo) => this.postArr = articolo)
+  }
+}
