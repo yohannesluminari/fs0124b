c45e04e3f139cf08b5283118b87a7588b785131c
yohannesluminari <y.luminari@gmail.com>
2024-03-20 17:03:17 +0100

    g3l3

--- m3/s1/g3/G3L3/src/app/app.component.ts
+++ m3/s1/g3/G3L3/src/app/app.component.ts
@@ -4,16 +4,29 @@ import { iArticolo } from './Models/articolo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  articoliArr: iArticolo[] = [];
+  randomArray: iArticolo[] = [];
 
-  postArr:iArticolo[] = [];
-
-  ngOnInit(){
-
+  ngOnInit() {
     fetch('../assets/db.json')
-    .then(articoli => articoli.json())
-    .then((articolo) => this.postArr = articolo)
+      .then((articoli) => articoli.json())
+      .then((articolo) => {
+        this.articoliArr = articolo.posts;
+        this.generaArrayRandom();
+      });
+  }
+
+  generaNumeroRandom() {
+    let index: number = Math.floor(Math.random() * this.articoliArr.length);
+    return index;
+  }
+
+  generaArrayRandom() {
+    for (let i = 0; i < 4; i++) {
+      this.randomArray.push(this.articoliArr[this.generaNumeroRandom()]);
+    }
   }
 }
