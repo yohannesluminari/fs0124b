87a8cb6716fda185610e15811daba5b151507828
yohannesluminari <y.luminari@gmail.com>
2024-03-25 16:46:46 +0100

    G1

---
+++ m4/s1/g1/L5/src/app/components/form/form.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export class FormComponent {
+showForm: any;
+toggleForm() {
+throw new Error('Method not implemented.');
+}
+isAvailable(_t3: any): any {
+throw new Error('Method not implemented.');
+}
+autoArr: any;
+
+}
