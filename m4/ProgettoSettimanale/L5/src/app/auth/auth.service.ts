4f7aacf65bd9df2c44441ef4d17c56645fe61a32
yohannesluminari <y.luminari@gmail.com>
2024-04-07 08:45:05 +0200

    favoriite Films noin funzionante

--- m4/ProgettoSettimanale/L5/src/app/auth/auth.service.ts
+++ m4/ProgettoSettimanale/L5/src/app/auth/auth.service.ts
@@ -8,97 +8,85 @@ import { environment } from '../../environments/environment.development';
 import { ILoginData } from '../models/i-login-data';
 import { IMovies } from '../models/i-movies';
 
-
-
 type AccessData = {
-  accessToken:string,
-  user:IUser
-}
+  accessToken: string,
+  user: IUser
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private users: IUser[] = [];
-  jwtHelper:JwtHelperService = new JwtHelperService()
+  jwtHelper: JwtHelperService = new JwtHelperService();
 
-  authSubject = new BehaviorSubject<IUser | null>(null) // null non sei ancolra collegato
+  authSubject = new BehaviorSubject<IUser | null>(null);
 
-  user$ = this.authSubject.asObservable() //contiene dati utenti loggatto o null
-  isLoggedIn$ = this.user$.pipe(map(user => !!user),
-  tap(user => this.syncIsLoggedIn = user)
-)
+  user$ = this.authSubject.asObservable();
+  isLoggedIn$ = this.user$.pipe(
+    map(user => !!user),
+    tap(user => this.syncIsLoggedIn = user)
+  );
 
-syncIsLoggedIn:boolean = false;
+  syncIsLoggedIn: boolean = false;
 
-  constructor(private Http:HttpClient, private router: Router  )
-    {
-      this.restoreUser()
-
+  constructor(private Http: HttpClient, private router: Router) {
+    this.restoreUser();
   }
 
   movies: (IMovies & { isLiked: boolean })[] = [];
-  resgisterUrl:string = environment.resgisterUrl
-  loginUrl:string =  environment.loginUrl
-  movieUrl:string = environment.movieUrl;
+  resgisterUrl: string = environment.resgisterUrl;
+  loginUrl: string = environment.loginUrl;
+  movieUrl: string = environment.movieUrl;
 
-  register(newUser:Partial<IUser>):Observable<AccessData>{
-
-    return this.Http.post<AccessData>(this.resgisterUrl, newUser)
+  register(newUser: Partial<IUser>): Observable<AccessData> {
+    return this.Http.post<AccessData>(this.resgisterUrl, newUser);
   }
 
-  login(loginData:ILoginData):Observable<AccessData>{
-
+  login(loginData: ILoginData): Observable<AccessData> {
     return this.Http.post<AccessData>(this.loginUrl, loginData)
-    .pipe(tap(data =>{
-      this.authSubject.next(data.user)
-      localStorage.setItem('accessData', JSON.stringify(data))
-
-      this.autoLogout(data.accessToken)
-    }))
+      .pipe(tap(data => {
+        this.authSubject.next(data.user);
+        localStorage.setItem('accessData', JSON.stringify(data));
+        this.autoLogout(data.accessToken);
+      }));
   }
 
-  logout(){
-    this.authSubject.next(null)
-      localStorage.removeItem('accessData')
-
-      this.router.navigate(['auth/login'])
-
+  logout() {
+    this.authSubject.next(null);
+    localStorage.removeItem('accessData');
+    this.router.navigate(['auth/login']);
   }
 
-  getAcccessToken():string{
-
-    const userJson = localStorage.getItem('accessData')
-      if(!userJson) return '';
-
-    const accessData:AccessData = JSON.parse(userJson)
-
-    if(this.jwtHelper.isTokenExpired(accessData.accessToken)) return '';
-
-      return accessData.accessToken
-
-  }
-  autoLogout(jwt:string){
-    const expDate = this.jwtHelper.getTokenExpirationDate(jwt) as Date //scadenza token
-    const expMs = expDate.getTime() - new Date().getTime() // numero millisecondi alla scanzdezna che parte appena loggato
-
-    setTimeout(()=>{
-      this.logout()
-    },expMs)
+  getAcccessToken(): string {
+    const userJson = localStorage.getItem('accessData');
+    if (!userJson) return '';
+    const accessData: AccessData = JSON.parse(userJson);
+    if (this.jwtHelper.isTokenExpired(accessData.accessToken)) return '';
+    return accessData.accessToken;
   }
 
-  // blocco il login se token scaduto
-  restoreUser(){
-    const userJson = localStorage.getItem('accessData')
-      if(!userJson) return;
+  autoLogout(jwt: string) {
+    const expDate = this.jwtHelper.getTokenExpirationDate(jwt) as Date;
+    const expMs = expDate.getTime() - new Date().getTime();
 
-      const accessData:AccessData = JSON.parse(userJson)
-
-      if(this.jwtHelper.isTokenExpired(accessData.accessToken)) return;
-
-      this.authSubject.next(accessData.user)
-      this.autoLogout(accessData.accessToken)
+    setTimeout(() => {
+      this.logout();
+    }, expMs);
   }
 
+  // Blocca il login se il token è scaduto
+  restoreUser() {
+    const userJson = localStorage.getItem('accessData');
+    if (!userJson) return;
+
+    const accessData: AccessData = JSON.parse(userJson);
+
+    if (this.jwtHelper.isTokenExpired(accessData.accessToken)) return;
+
+    this.authSubject.next(accessData.user);
+    this.autoLogout(accessData.accessToken);
+  }
 
   getAllUsers(): Observable<IUser[]> {
     return this.Http.get<IUser[]>(environment.userUrl);
@@ -112,14 +100,28 @@ syncIsLoggedIn:boolean = false;
     return this.users;
   }
 
-
   updateFavoriteMovies(userId: number, favoriteMovies: IMovies[]): void {
     const userIndex = this.users.findIndex(user => user.id === userId);
     if (userIndex !== -1) {
       this.users[userIndex].FavouriteFilms = favoriteMovies;
       localStorage.setItem('users', JSON.stringify(this.users));
     }
-}
+  }
+
+  removeFromFavorites(userId: number, movieId: number): void {
+    const userData = localStorage.getItem('users');
+    if (userData) {
+      const users: IUser[] = JSON.parse(userData);
+      const user = users.find(user => user.id === userId);
+      if (user && user.FavouriteFilms) {
+        const index = user.FavouriteFilms.findIndex(movie => movie.id === movieId);
+        if (index !== -1) {
+          user.FavouriteFilms.splice(index, 1);
+          localStorage.setItem('users', JSON.stringify(users));
+        }
+      }
+    }
+  }
 
   getFavoriteMovies(userId: number): IMovies[] | undefined {
     const userData = localStorage.getItem('users');
@@ -130,16 +132,4 @@ syncIsLoggedIn:boolean = false;
     }
     return undefined;
   }
-
-  removeFromFavorites(userId: number, movieId: number): void {
-    const user = this.users.find(user => user.id === userId);
-    if (user && user.FavouriteFilms) {
-      const index = user.FavouriteFilms.findIndex(movie => movie.id === movieId);
-      if (index !== -1) {
-        user.FavouriteFilms.splice(index, 1);
-        // Aggiorna i dati dell'utente nel localStorage o sul server, se necessario
-        // Esempio: localStorage.setItem('users', JSON.stringify(this.users));
-      }
-    }
-}
 }
--- m4/ProgettoSettimanale/L5/src/app/favourite-movies/favourite-movies.component.ts
+++ m4/ProgettoSettimanale/L5/src/app/favourite-movies/favourite-movies.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IMovies } from '../models/i-movies';
+import { AuthService } from '../auth/auth.service';
+import { IUser } from '../models/i-user';
 
 @Component({
   selector: 'app-favourite-movies',
   templateUrl: './favourite-movies.component.html',
-  styleUrl: './favourite-movies.component.scss'
+  styleUrls: ['./favourite-movies.component.scss']
 })
-export class FavouriteMoviesComponent {
+export class FavouriteMoviesComponent implements OnInit {
+  favouriteMovies: IMovies[] = [];
+  currentUser: IUser | null = null;
 
+  constructor(private authSvc: AuthService) { }
+
+  ngOnInit(): void {
+    this.authSvc.user$.subscribe(user => {
+      if (user) {
+        this.currentUser = user;
+        const userId = user.id;
+        this.favouriteMovies = this.authSvc.getFavoriteMovies(userId) || [];
+      }
+    });
+  }
+
+  removeFromFavorites(movieId: number): void {
+    if (this.currentUser) {
+      const userId = this.currentUser.id;
+      this.authSvc.removeFromFavorites(userId, movieId);
+      this.favouriteMovies = this.authSvc.getFavoriteMovies(userId) || [];
+    }
+  }
 }
